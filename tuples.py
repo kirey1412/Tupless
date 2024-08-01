658ed02842e4ba6a4f4cbcbcb97ad1e0c904bc96
kirey1412 <kireykayana14@gmail.com>
2024-08-01 20:01:37 +0700

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,20 @@
+#tuples
+a=(12,"Spring","London")
+
+#packing
+hno,apt,city=a
+
+#unpacking
+c=123, "Kirey"
+print(c)
+
+#nestedtuples
+x=((2012, "Michelle", "Bali"),(11,"Kirey","Jakarta"))
+
+#slicingtuples
+g=('K','I','R','E','Y')
+print(g[0:4])
+
+#immutable
+#g[1]='L'
+#print(g)
